dd6da59560fc6c6540c929cb7d3bb281f601868b
Darwin Biler <buonzz@gmail.com>
2016-01-01 23:39:58 +0800

    Update setup_ruby.sh

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -2,6 +2,5 @@
 
 sudo apt-get update
 
-sudo apt-get install ruby1.9.1 ruby1.9.1-dev \
-  rubygems1.9.1 irb1.9.1 ri1.9.1 rdoc1.9.1 \
-  build-essential libopenssl-ruby1.9.1 libssl-dev zlib1g-dev -y
+sudo apt-get install ruby-full
+sudo apt-get install rubygems
