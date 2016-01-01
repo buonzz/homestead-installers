59c4381064d4afda8625a23c9ea93d3f71127153
Darwin Biler <buonzz@gmail.com>
2016-01-01 23:56:37 +0800

    Update setup_ruby.sh

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
-sudo apt-get update
-
-sudo apt-get install ruby-full
-sudo apt-get install rubygems
+sudo apt-get -y update
+sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
+cd /tmp
+wget https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.0.tar.gz
+tar -xvzf ruby-2.3.0.tar.gz
+cd ruby-2.3.0/
+./configure --prefix=/usr/local
+make
+sudo make install
