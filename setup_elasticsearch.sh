44e9dbccb57c908b84b5fb4182154e2b8f824be0
Darwin Biler <buonzz@gmail.com>
2016-01-31 08:57:15 +0800

    Create setup_elasticsearch.sh

---
+++ setup_elasticsearch.sh
@@ -0,0 +1,6 @@
+wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
+sudo apt-get update && sudo apt-get install elasticsearch
+
+sudo update-rc.d elasticsearch defaults 95 10
+sudo /etc/init.d/elasticsearch start
