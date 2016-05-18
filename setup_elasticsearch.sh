7d616468c4e4391da6ec7c310f0c7784715c14a9
buonzz <buonzz@gmail.com>
2016-05-18 18:38:45 +0800

    opened firewall

--- setup_elasticsearch.sh
+++ setup_elasticsearch.sh
@@ -2,5 +2,9 @@ wget -qO - https://packages.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add 
 echo "deb http://packages.elastic.co/elasticsearch/2.x/debian stable main" | sudo tee -a /etc/apt/sources.list.d/elasticsearch-2.x.list
 sudo apt-get update && sudo apt-get install elasticsearch
 
+sudo echo "network.host: 0.0.0.0" >> /etc/elasticsearch/elasticsearch.yml
+sudo iptables -I INPUT -p tcp --dport 9200 -j ACCEPT
+sudo iptables-save
+
 sudo update-rc.d elasticsearch defaults 95 10
 sudo /etc/init.d/elasticsearch start
