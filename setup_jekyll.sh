f778a7a7f845897aaa61cc5ee3567c18ab487563
buonzz <buonzz@gmail.com>
2016-05-19 09:36:44 +0800

    fixed kibana not installing

--- setup_jekyll.sh
+++ setup_jekyll.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# ask user to invoke setup_ruby and setup_nodejs first
-
 sudo gem install jekyll
 
 # install common plugins
--- setup_kibana.sh
+++ setup_kibana.sh
@@ -5,7 +5,7 @@ gunzip kibana-4.3.1-linux-x64.tar.gz
 tar -xvf kibana-4.3.1-linux-x64.tar
 sudo mkdir /opt/kibana
 sudo cp -Rrvf kibana-4.3.1-linux-x64/* /opt/kibana/
-sudo cd /etc/init.d/
+cd /etc/init.d/
 wget https://gist.githubusercontent.com/thisismitch/8b15ac909aed214ad04a/raw/bce61d85643c2dcdfbc2728c55a41dab444dca20/kibana4
 sudo chmod +x /etc/init.d/kibana4
 
