9dd294b6e30879c18a15fd4084287a1c65d8ba0a
Darwin Biler <buonzz@gmail.com>
2018-02-10 21:55:39 +0800

    use rbenv

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-sudo apt-get -y update
-sudo apt-get -y install build-essential zlib1g-dev libssl-dev libreadline6-dev libyaml-dev
-cd /tmp
-wget https://cache.ruby-lang.org/pub/ruby/2.3/ruby-2.3.0.tar.gz
-tar -xvzf ruby-2.3.0.tar.gz
-cd ruby-2.3.0/
-./configure --prefix=/usr/local
-make
-sudo make install
+sudo apt-get update
+sudo apt-get install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+git clone https://github.com/rbenv/rbenv.git ~/.rbenv
+echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(rbenv init -)"' >> ~/.bashrc
+source ~/.bashrc
+git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+rbenv install 2.3.1
+rbenv global 2.3.1
