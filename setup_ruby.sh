c830ac6f89b42271c21ecbb7eb55fec1fc09cc63
Darwin Biler <buonzz@gmail.com>
2018-02-10 22:01:46 +0800

    place the correct bashrc

--- setup_ruby.sh
+++ setup_ruby.sh
@@ -5,7 +5,7 @@ sudo apt-get install -y autoconf bison build-essential libssl-dev libyaml-dev li
 git clone https://github.com/rbenv/rbenv.git ~/.rbenv
 echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(rbenv init -)"' >> ~/.bashrc
-source ~/.bashrc
 git clone https://github.com/rbenv/ruby-build.git ~/.rbenv/plugins/ruby-build
+source ~/.bashrc
 rbenv install 2.3.1
 rbenv global 2.3.1
